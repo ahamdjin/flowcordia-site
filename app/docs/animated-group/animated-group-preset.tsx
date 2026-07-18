5ed486f68d74aacc8aa8dcd7659ac3dc3a408b29
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-07-19 04:30:54 +0500

    content(site): show Flowcordia product surfaces in animated grid

--- app/docs/animated-group/animated-group-preset.tsx
+++ app/docs/animated-group/animated-group-preset.tsx
@@ -7,23 +7,23 @@ export function AnimatedGroupPreset() {
       preset='scale'
     >
       <img
-        src='https://images.beta.cosmos.so/fc6fdd93-552c-47e6-98aa-b8fb3ba070a2?format=jpeg'
-        alt='impressionist painting, uploaded to Cosmos'
+        src='/flowcordia-canvas.svg'
+        alt='Flowcordia Studio canvas'
         className='h-auto w-full rounded-[4px]'
       />
       <img
-        src='https://images.beta.cosmos.so/cb674d14-ebd1-4408-bab1-79df895017b6?format=jpeg'
-        alt='impressionist painting, uploaded to Cosmos'
+        src='/flowcordia-source.svg'
+        alt='Flowcordia typed source view'
         className='h-auto w-full rounded-[4px]'
       />
       <img
-        src='https://images.beta.cosmos.so/e5a6c3ed-82ad-4084-9a11-1eccd7bc91aa?format=jpeg'
-        alt='impressionist painting, uploaded to Cosmos'
+        src='/flowcordia-proposal.svg'
+        alt='Flowcordia Git proposal view'
         className='h-auto w-full rounded-[4px]'
       />
       <img
-        src='https://images.beta.cosmos.so/4d02a1e7-d1f2-4575-86a9-bed243e59132?format=jpeg'
-        alt='impressionist painting, uploaded to Cosmos'
+        src='/flowcordia-run.svg'
+        alt='Flowcordia workflow run view'
         className='h-auto w-full rounded-[4px]'
       />
     </AnimatedGroup>
