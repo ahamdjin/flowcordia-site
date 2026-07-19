377f60e77e9bb48cd1b5203b2d18550b9edffbc7
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-07-20 04:19:56 +0500

    Give Card 1 the full homepage frame

--- app/page.tsx
+++ app/page.tsx
@@ -146,7 +146,7 @@ export default function Motion() {
           </span>
         </section>
         <section className='mx-auto max-w-3xl py-32'>
-          <CardExampleLanding>
+          <CardExampleLanding className='p-0 [&>div]:h-[560px] sm:[&>div]:h-[500px]'>
             <MorphingDialogBasicOne />
           </CardExampleLanding>
         </section>
