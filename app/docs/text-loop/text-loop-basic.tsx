5c1ed1f06290ff12c2bdcfe3cb55de8b728cc3f2
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-07-20 16:42:10 +0500

    Connect Card 7 to the function workbench

--- app/docs/text-loop/text-loop-basic.tsx
+++ app/docs/text-loop/text-loop-basic.tsx
@@ -1,12 +1,5 @@
-import { TextLoop } from '@/components/core/text-loop';
+import { FunctionWorkbench } from '@/components/website/function-workbench';
 
 export function TextLoopBasic() {
-  return (
-    <TextLoop className='font-mono text-sm'>
-      <span>Build on the canvas</span>
-      <span>Extend with typed functions</span>
-      <span>Review the change in GitHub</span>
-      <span>Run the exact version</span>
-    </TextLoop>
-  );
+  return <FunctionWorkbench />;
 }
