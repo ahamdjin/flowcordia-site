73081ba3ea144d4ad8163500e2e0481d754ac2dd
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-07-20 05:29:23 +0500

    Replace Card 2 with the reviewed delivery workflow

--- app/docs/animated-group/animated-group-preset.tsx
+++ app/docs/animated-group/animated-group-preset.tsx
@@ -1,31 +1,5 @@
-import { AnimatedGroup } from '@/components/core/animated-group';
+import { ReviewedDeliveryWorkflow } from '@/components/website/reviewed-delivery-workflow';
 
 export function AnimatedGroupPreset() {
-  return (
-    <AnimatedGroup
-      className='grid grid-cols-2 gap-4 p-8 md:grid-cols-3 lg:grid-cols-4'
-      preset='scale'
-    >
-      <img
-        src='/flowcordia-canvas.svg'
-        alt='Flowcordia Studio canvas'
-        className='h-auto w-full rounded-[4px]'
-      />
-      <img
-        src='/flowcordia-source.svg'
-        alt='Flowcordia typed source view'
-        className='h-auto w-full rounded-[4px]'
-      />
-      <img
-        src='/flowcordia-proposal.svg'
-        alt='Flowcordia Git proposal view'
-        className='h-auto w-full rounded-[4px]'
-      />
-      <img
-        src='/flowcordia-run.svg'
-        alt='Flowcordia workflow run view'
-        className='h-auto w-full rounded-[4px]'
-      />
-    </AnimatedGroup>
-  );
+  return <ReviewedDeliveryWorkflow />;
 }
