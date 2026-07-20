94a393aab6a867ee6ba366626620a6dd74b9e48a
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-07-20 16:45:13 +0500

    Let the function workbench own the Card 7 frame

--- app/docs/text-loop/text-loop-basic.tsx
+++ app/docs/text-loop/text-loop-basic.tsx
@@ -1,5 +1,36 @@
 import { FunctionWorkbench } from '@/components/website/function-workbench';
 
 export function TextLoopBasic() {
-  return <FunctionWorkbench />;
+  return (
+    <>
+      <style>{`
+        .flowcordia-card-seven {
+          height: 820px;
+          width: calc(100% + 4rem);
+          margin-left: -2rem;
+        }
+
+        div:has(> .flowcordia-card-seven) {
+          height: 820px !important;
+          align-items: stretch !important;
+          justify-content: stretch !important;
+        }
+
+        @media (min-width: 768px) {
+          .flowcordia-card-seven {
+            height: 590px;
+            width: calc(100% + 10rem);
+            margin-left: -5rem;
+          }
+
+          div:has(> .flowcordia-card-seven) {
+            height: 590px !important;
+          }
+        }
+      `}</style>
+      <div className='flowcordia-card-seven'>
+        <FunctionWorkbench />
+      </div>
+    </>
+  );
 }
